69bcd62121e082075d6b6b6cc66a82307db4e37b
Georgiy Sobolev <goshasobolev2003@gmail.com>
2024-08-06 18:39:31 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketsMap = {};
+  let openBrackets = new Set();
+  let closeBrackets = new Set();
+
+  bracketsConfig.forEach((pair) => {
+    bracketsMap[pair[1]] = pair[0];
+    openBrackets.add(pair[0]);
+    closeBrackets.add(pair[1]);
+  });
+
+  for (let char of str) {
+    if (openBrackets.has(char)) {
+      if (closeBrackets.has(char) && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
